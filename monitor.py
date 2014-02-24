ea3e9a543b7137d3209b758d5f9fe5d830cd54c6
mymoflx <lilexian2005@163.com>
2014-02-25 00:05:51 +0800

    add monitor and progress config

--- monitor.py
+++ monitor.py
@@ -3,31 +3,27 @@ __author__ = 'lilx at 14-2-13'
 import time
 import os
 import gevent
+import json
+from datetime import datetime
 
 
 class Monitor(object):
     """
     log file monitor
     """
-    def __init__(self, file_path, interval, lines, position, timestamp, with_history):
+    def __init__(self, cfg):
         """
-        file_path : str the monitor file path
-        interval : int the rate to check the file
-        lines : int line count read at a time
-        position : int the read position of last time
-        timestamp : float the last read timestamp
         with_history : bool if check the history file
         """
-        self.file_path = file_path
-        self.interval = interval
-        self.lines = lines
-        self.position = position
-        self.timestamp = timestamp
-        self.with_history = with_history
+        self.cfg = cfg
+        self.with_history = self.cfg.get('with_history')
+        self.file_path = self.cfg['file_path']
+        self.progress_path = ''
+        self.progress = {}
         self.handlers = []
-        self.file_id = None
-        self.file = None
+        self.listeners = {}
         self.running = False
+        self.changes = 0
 
     def register_handler(self, handler):
         if handler not in self.handlers:
@@ -38,16 +34,58 @@ class Monitor(object):
             handler(content)
 
     def report_progress(self, listener):
-        pass
+        pre_position = self.progress[listener.file_path].get('position', 0)
+        cur_position = listener.progress.get('position', 0)
+        cur_timestamp = listener.progress.get('timestamp', 0)
+        self.progress[listener.file_path]['timestamp'] = cur_timestamp
+        self.progress[listener.file_path]['position'] = cur_position
+        if pre_position != cur_position:
+            self.changes += 1
+            if self.changes >= self.cfg['change_to_save']:
+                self.save_progress()
+                self.changes = 0
+        elif listener.file_path != self.file_path:
+            listener.stop()
 
     def start(self):
-        if not self.running:
-            self.running = True
-            self.__monitor()
+        self.load_progress()
+        self.bind_listener()
+        self.running = True
+        for listener in self.listeners.values():
+            listener.start()
 
-    def __monitor(self):
-        while self.running:
-            pass
+    def stop(self):
+        for listener in self.listeners.values():
+            listener.stop()
+        self.save_progress()
+
+    def load_progress(self):
+        p = os.path.join('data', 'progress_monitor_' + self.cfg['name'])
+        self.progress_path = p
+        if os.path.isfile(p):
+            f = open(p, 'rb')
+            self.progress = json.load(f)
+        if self.file_path not in self.progress:
+            self.progress[self.file_path] = {
+                'position': 0
+            }
+
+    def save_progress(self):
+        p = os.path.join('data', 'progress_monitor_' + self.cfg['name'])
+        json.dump(self.progress, open(p, 'wb'))
+
+    def bind_listener(self):
+        main_progress = self.progress[self.file_path]
+        timestamp = main_progress.get('timestamp')
+        if timestamp and self.with_history:
+            pt = datetime.fromtimestamp(timestamp)
+            history_log_path = self.file_path + '.' + pt.strftime(self.cfg['suffix_date_format'])
+            if history_log_path not in self.progress:
+                self.progress[history_log_path] = {
+                    'position': 0
+                }
+        for file_path, progress in self.progress.iteritems():
+            self.listeners[file_path] = Listener(self, file_path, progress, {})
 
 
 class Listener(object):
@@ -66,7 +104,7 @@ class Listener(object):
     def start(self):
         if not self.running:
             self.running = True
-            self.__listen()
+            gevent.spawn(self.__listen)
 
     def stop(self):
         self.running = False
